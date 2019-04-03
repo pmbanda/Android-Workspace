5fc3b76f8a8a940fd483a8f8dbc4b4886ccc6d8f
Peter Eugene Mbanda <pmbandaft@gmail.com>
2019-04-03 01:44:40 -0500

    Implement text changed listener

--- DevelopersGuideGoogle/CriminalIntent/app/src/main/java/com/android/criminalintent/CrimeFragment.java
+++ DevelopersGuideGoogle/CriminalIntent/app/src/main/java/com/android/criminalintent/CrimeFragment.java
@@ -1,7 +1,52 @@
 package com.android.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mCrime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        return v;
+
+    }
 }
