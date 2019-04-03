c42d8889d03f0e2f1b9bc033626f55e6897237f2
Peter Eugene Mbanda <pmbandaft@gmail.com>
2019-04-03 02:48:11 -0500

    Implement Fragment manager as container for the fragments

--- DevelopersGuideGoogle/CriminalIntent/app/src/main/java/com/android/criminalintent/CrimeActivity.java
+++ DevelopersGuideGoogle/CriminalIntent/app/src/main/java/com/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.android.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,17 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        // Add the fragment manager
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        // if fragment is null
+        if (fragment == null) {
+
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
